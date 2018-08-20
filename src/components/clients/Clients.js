e4ca7de55101a000fd421466efe4701af4156ae9
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-08-20 13:22:09 -0400

    fixed

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -6,24 +6,21 @@ import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import Spinner from '../layout/Spinner';
 
-
 class Clients extends Component {
-  render () {
+
+
+  render() {
     const { clients } = this.props;
 
-    if(clients) {
+    if (clients) {
       return (
         <div>
           <div className="row">
             <div className="col-md-6">
               <h2>
                 {' '}
-                 <i class="fas fa-database" /> Data Set:{' '}
+                <i class="fas fa-database" /> Data Set:{' '}
               </h2>
-            </div>
-            <div className="col-md-6">
-              <h5 className="text-right text-secondary">
-              </h5>
             </div>
           </div>
 
@@ -41,25 +38,27 @@ class Clients extends Component {
                 <tr key={client.id}>
                   <td>
                     {client.product}
+
                   </td>
                   <td>{client.color}</td>
                   <td>{client.price}</td>
+
                   <td>
                     <Link
                       to={`/client/${client.id}`}
-                      className="btn btn-secondary btn-sm">
-                      <i className="fas fa-arrow-circle-right" /> Details
+                      className="btn btn-secondary btn-sm"
+                    >
+                      <i className="fas fa-arrow-circle-right" /> Update
                     </Link>
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
-
         </div>
       );
     } else {
-      <Spinner />;
+      return <Spinner />;
     }
   }
 }
@@ -75,26 +74,3 @@ export default compose(
     clients: state.firestore.ordered.clients
   }))
 )(Clients);
-
-
-
-// const clients = [
-//   {
-//     id: "56473829",
-//     product: "Apple iphone 6s",
-//     color: "Dark Grey",
-//     price: "CHF 769"
-//   },
-//   {
-//     id: "123456789",
-//     product: "Samsung Galaxy S8",
-//     color: "Midnight Black",
-//     price: "CHF 569"
-//   },
-//   {
-//     id: "98765e11",
-//     product: "Huawei P9",
-//     color: "Mystic Silver",
-//     price: "CHF 272"
-//   },
-// ];
