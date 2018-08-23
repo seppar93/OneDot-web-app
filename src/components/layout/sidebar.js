0358d2ba72b208ed808b5af8d05f6150882213cb
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-08-22 20:50:49 -0400

    sidebarerror

--- src/components/layout/sidebar.js
+++ src/components/layout/sidebar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default () => {
-  return (
-    <Link to="/client/add" className="btn btn-success btn-block">
-      <i className="fas fa-plus" /> New
-    </Link>
-  );
-};
+class Sidebar extends React.Component{
+	render () {
+		return(
+    		<Link to="/client/add" className="btn btn-success btn-block">
+      			<i className="fas fa-plus" /> New
+    		</Link>
+  		);
+	}
+}
+export default Sidebar
